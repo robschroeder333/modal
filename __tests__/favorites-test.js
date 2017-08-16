7a7de2c4f61ce7b27e8b6ea8a7e2f70be392b5a9
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-08-16 09:34:17 -0400

    docs: add final comments

--- __tests__/favorites-test.js
+++ __tests__/favorites-test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Favorites from '../client/react/components/modals/favorites.js';
 import Renderer from 'react-test-renderer';
 
+/*
+Not experienced with testing on the front end, but want to learn,
+as it is super helpful/useful.
+*/
 test('add new input for favorite', () => {
 
 });
